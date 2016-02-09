23918c381c95a9f1aef18cc5cc9447f420f46e5b
Johannes Hilden <hildenjohannes@gmail.com>
2016-02-09 09:02:00 +0300

    add files

---
+++ json-combinator/Fri/Json/Parser.hs
@@ -0,0 +1,130 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase        #-}
+module Fri.Json.Parser where
+
+import Control.Applicative        ( (<$>), (<*), (*>), (<|>) )
+import Data.Monoid
+import Data.Attoparsec.Combinator ( lookAhead )
+import Data.Attoparsec.Text
+import Data.Char                  ( chr ) 
+import Data.Text                  ( Text, pack, unpack )
+
+import qualified Data.Map.Strict as H
+
+type Dictionary = H.Map Text Json
+
+-- | Data type to represent a JSON value.
+data Json = Object !Dictionary  | Array ![Json]  | Number !Double 
+          | String !Text        | Boolean !Bool  | Null
+    deriving (Show, Eq)
+
+--
+-- Various helpers
+--
+
+oneOf :: String -> Parser Char
+oneOf = satisfy . inClass
+
+maybeOption :: Parser a -> Parser (Maybe a)
+maybeOption p = option Nothing (Just <$> p)
+
+-- | Parse a string literal, i.e., zero or more characters enclosed in 
+--   double quotes.
+literal :: Parser Text
+literal = pack <$> chars
+  where
+    chars = char '"' *> manyTill validChar (char '"')
+    validChar = special 
+            <|> unicode 
+            <|> notChar '\\'
+    -- Escaped special character
+    special = char '\\' *> oneOf "\"\\/bfnrt"
+    -- Unicode escape sequence
+    unicode :: Parser Char
+    unicode = do
+        string "\\u"
+        code <- count 4 hexDigit
+        return $ chr $ read $ "0x" ++ code
+    -- A single hexadecimal digit
+    hexDigit = oneOf "0123456789abcdefABCDEF"
+
+-- | Decode a JSON string literal.
+jsonString :: Parser Json
+jsonString = String <$> literal 
+
+-- | Decode a JSON number.
+jsonNumber :: Parser Json
+jsonNumber = do
+    sign <- maybeOption (char '-')
+    int  <- return <$> char '0' <|> many1 digit
+    frac <- fractional
+    pow  <- option 0 exponent
+    let digits = read (int <> frac) * 10 ^ pow 
+    return $ Number $ case sign of
+      Nothing -> digits
+      Just _  -> negate digits
+  where
+    -- Fractional part
+    fractional = maybeOption (char '.') 
+        >>= \case 
+          Nothing -> return []
+          Just _  -> many1 digit >>= return <$> (:) '.'
+    -- Exponent (scientific notation)
+    exponent = do
+        prefix <- oneOf "eE"
+        sign   <- maybeOption (oneOf "+-")
+        digits <- read <$> many1 digit
+        return $ case sign of
+          Just '-' -> negate digits
+          _        -> digits
+
+-- | Decode a boolean.
+jsonBoolean :: Parser Json
+jsonBoolean = true <|> false
+  where
+    true  = "true"  *> return (Boolean True)
+    false = "false" *> return (Boolean False)
+
+-- | Decode a null value.
+jsonNull :: Parser Json
+jsonNull = "null" *> return Null
+
+-- | Decode a JSON object.
+jsonObject :: Parser Json
+jsonObject = do
+    char '{'
+    body <- padded keyValuePair `sepBy` char ','
+    char '}'
+    return $ Object $ H.fromList body
+  where
+    keyValuePair :: Parser (Text, Json)
+    keyValuePair = do
+        key <- literal
+        padded (char ':')
+        value <- jsonValue
+        return (key, value)
+
+-- | Decode a JSON array.
+jsonArray :: Parser Json
+jsonArray = do
+    char '['
+    values <- padded jsonValue `sepBy` char ','
+    char ']'
+    return $ Array values
+
+-- | Decode a JSON value.
+jsonValue :: Parser Json
+jsonValue = jsonObject
+        <|> jsonArray
+        <|> jsonNumber
+        <|> jsonString
+        <|> jsonBoolean
+        <|> jsonNull
+
+-- | Decode JSON data with possible leading blank space.
+json :: Parser Json
+json = skipSpace *> jsonValue 
+
+padded :: Parser a -> Parser a
+padded match = skipSpace *> match <* skipSpace
+
---
+++ json-combinator/README.md
@@ -0,0 +1,74 @@
+This is a simple example of how combinator parsing works in Haskell. 
+
+> Don't use this parser in production code. You may instead want to have a look at [Aeson](https://hackage.haskell.org/package/aeson) for a mature, efficient and well-maintained JSON parsing library.
+
+```
+  build-depends:       base >=4.6, attoparsec >=0.13, text, containers
+```
+
+First, we need some imports.
+
+```haskell
+import Data.Attoparsec.Text
+import Data.Text
+
+import qualified Data.Map.Strict as H
+```
+
+Let's introduce a simple algebraic data type to represent JSON data in Haskell-land. Looking at [json.org](http://json.org/), we can see that a JSON value is exactly one of the following; a *string*, a *number*, an *object*, an *array*, *true*, *false*, or *null*.
+
+```haskell
+data Json = Object  !Dictionary  
+          | Array   ![Json]  
+          | Number  !Double 
+          | String  !Text        
+          | Boolean !Bool  
+          | Null
+    deriving (Show, Eq)
+```
+
+Most of this is straightforward. It makes sense to combine *true* and *false* into a single data constructor. `Dictionary` is a type synonym for a `Map` with `Text` keys and JSON value entries, defined as
+
+```haskell
+type Dictionary = H.Map Text Json
+```
+
+We will need combinators to parse each of these separately.
+
+```haskell
+jsonString, jsonNumber, jsonBoolean, jsonNull, jsonObject, jsonArray, jsonValue :: Parser Json
+```
+
+The top-level value parser is the easiest to implement since it, by definition, should match exactly on of the other types.
+
+```haskell
+jsonValue = jsonObject
+        <|> jsonArray
+        <|> jsonNumber
+        <|> jsonString
+        <|> jsonBoolean
+        <|> jsonNull
+```
+
+```haskell
+-- | Parse a string literal, i.e., zero or more characters enclosed in double quotes.
+literal :: Parser Text
+literal = ...
+```
+
+Parsing a JSON string is now as easy as lifting the String constructor into the Parser monad and ... 
+
+```haskell
+-- | Decode a JSON string literal.
+jsonString :: Parser Json
+jsonString = String <$> literal 
+```
+
+```haskell
+λ> :t String
+String :: Text -> Json
+λ> :t literal
+literal :: Parser Text
+λ> :t fmap String literal      -- == String <$> literal
+fmap String literal :: Parser Json
+```
